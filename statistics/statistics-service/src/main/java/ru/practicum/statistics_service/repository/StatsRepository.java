a0659b50f9c19d089cd6961ad2f8142447fa0c53
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-03-29 01:32:22 +0300

    StatsRepository - добавление сортировки по убыванию к-ва обращений

--- statistics/statistics-service/src/main/java/ru/practicum/statistics_service/repository/StatsRepository.java
+++ statistics/statistics-service/src/main/java/ru/practicum/statistics_service/repository/StatsRepository.java
@@ -10,18 +10,18 @@ import java.util.List;
 
 public interface StatsRepository extends  JpaRepository<Hit, Long> {
     @Query("select new ru.practicum.statistics_service.controller.HitOutcomeDto(h.app, h.uri, count(h.id)) from Hit as h " +
-    "where h.timestamp >= :start and h.timestamp <= :end group by h.app, h.uri")
+    "where h.timestamp >= :start and h.timestamp <= :end group by h.app, h.uri order by count(h.id) desc ")
     List<HitOutcomeDto> findByStartAndEnd(LocalDateTime start, LocalDateTime end);
 
     @Query("select new ru.practicum.statistics_service.controller.HitOutcomeDto(h.app, h.uri, count(h.id)) from Hit as h " +
-            "where h.timestamp >= :start and h.timestamp <= :end and h.uri in :uris group by h.app, h.uri")
+            "where h.timestamp >= :start and h.timestamp <= :end and h.uri in :uris group by h.app, h.uri order by count(h.id) desc")
     List<HitOutcomeDto> findByStartAndEndAndUri(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("select new ru.practicum.statistics_service.controller.HitOutcomeDto(h.app, h.uri, count(distinct(h.ip))) from Hit as h " +
-            "where h.timestamp >= :start and h.timestamp <= :end group by h.app, h.uri")
+            "where h.timestamp >= :start and h.timestamp <= :end group by h.app, h.uri order by count(distinct(h.ip)) desc ")
     List<HitOutcomeDto> findByStartAndEndAndIp(LocalDateTime start, LocalDateTime end);
 
     @Query("select new ru.practicum.statistics_service.controller.HitOutcomeDto(h.app, h.uri, count(distinct(h.ip))) from Hit as h " +
-            "where h.timestamp >= :start and h.timestamp <= :end and h.uri in :uris group by h.app, h.uri")
+            "where h.timestamp >= :start and h.timestamp <= :end and h.uri in :uris group by h.app, h.uri order by count(distinct(h.ip)) desc")
     List<HitOutcomeDto> findByStartAndEndAndUriAndIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
